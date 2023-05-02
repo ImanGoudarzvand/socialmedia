7b5a3eb4abd5dece10e2b22bf6ca73bde7159b12
ImanGoudarzvand <im.goudarz@gmail.com>
2023-05-02 23:47:54 +0330

    register and auth api  submitted through api app

--- socialmedia/api/urls.py
+++ socialmedia/api/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
 
+    path('users/', include(('socialmedia.users.urls', 'users'))),
+    path('auth/', include(('socialmedia.authentication.urls', 'authentication'))),
+
 ] 
