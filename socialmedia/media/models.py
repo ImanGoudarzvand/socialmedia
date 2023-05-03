a4f13352923b4f76fcd04db9380d09ee504de564
ImanGoudarzvand <im.goudarz@gmail.com>
2023-05-03 22:44:35 +0330

    media models added

--- socialmedia/media/models.py
+++ socialmedia/media/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from rest_framework.serializers import ValidationError
+from socialmedia.common.models import BaseModel
+class Post(BaseModel):
+
+    slug = models.SlugField(primary_key=True, max_length=100)
+
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=1000)
+    author = models.ForeignKey(to = settings.AUTH_USER_MODEL, on_delete = models.SET_NULL, null=True)
+
+
+    def __str__(self):
+        return self.slug
+
+
+class Connection(models.Model):
+    subscriber = models.ForeignKey(to = settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name="subscribers")
+    target = models.ForeignKey(to = settings.AUTH_USER_MODEL, on_delete = models.CASCADE, related_name="targets")
+
+    class Meta:
+        unique_together = ('subscriber', 'target')
+
+    def clean(self):
+        if self.subscriber == self.target:
+            raise ValidationError({"subscriber": ("subscriber cannot be equal to target")})
+
+    def __str__(self):
+        return f"{self.subscriber.username}- {self.target.username}"
